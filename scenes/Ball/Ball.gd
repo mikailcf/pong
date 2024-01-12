693733e874e1908441532193116d8e51def36dcc
Mikail Freitas <mikailcf@gmail.coml>
2024-01-12 19:47:38 -0300

    bouncing ball

--- scenes/Ball/Ball.gd
+++ scenes/Ball/Ball.gd
@@ -1,12 +1,26 @@
 extends AnimatableBody2D
 
 const SPEED = 300.0
+var _speed_modifier := 1.0
 	
 func _ready():
-	constant_linear_velocity = Vector2(SPEED, 0.0)
+	constant_linear_velocity = Vector2(-SPEED, 0.0)
 
 func _physics_process(delta):
 	move_and_collide(constant_linear_velocity * delta)
+	
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		var x := constant_linear_velocity.x
+		var y := body.get_hit_range(global_position.y) as float
 
-func _on_area_2d_area_entered(area):
-	pass # Replace with function body.
+		constant_linear_velocity = Vector2(-x, y * SPEED)
+
+func _on_area_2d_area_entered(area: Node2D):
+	var velx = constant_linear_velocity.x
+	var vely = constant_linear_velocity.y
+	
+	if area.is_in_group("hor_bound"):
+		constant_linear_velocity = Vector2(velx, -vely)
+	if area.is_in_group("vert_bound"):
+		constant_linear_velocity = Vector2(-velx, vely)
--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,7 +1,19 @@
 extends CharacterBody2D
 
 @export var speed := 200.0
+@onready var hitbox: RectangleShape2D = $Area2D/CollisionShape2D.shape
 
 func _physics_process(delta):
 	velocity = speed * InputControl.vector("ui_left", "ui_right", "ui_up", "ui_down")
 	move_and_slide()
+
+func get_hit_range(hit_y: float) -> float:
+	var top = global_position.y - hitbox.size.y/2.0
+	var bottom = global_position.y + hitbox.size.y/2.0
+	
+	if hit_y < top:
+		return -1.0
+	if hit_y > bottom:
+		return 1.0
+
+	return 2.0 * ((hit_y - top)/(bottom - top) - 0.5)
